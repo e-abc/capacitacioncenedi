8a6b149d07ca64a3f4d86cca1106abb5c71d82ac
Daniel <dannielarriola@gmail.com>
2018-04-15 17:33:37 -0300

    data api

--- local/example/view.php
+++ local/example/view.php
@@ -2,13 +2,17 @@
 
 require_once('../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
-global $PAGE, $OUTPUT;
+global $PAGE, $OUTPUT, $DB;
 
 admin_externalpage_setup('test');
 
 echo $OUTPUT->header();
 
 $config = get_config('local_example', 'config');
+
+$user = $DB->get_record('user', array('id' => 1));
+
+var_dump($user);
 
 echo $config;
 
